bc10da8830b47915df1a1d43145e8462b61a5590
Terrance Li <terranceli.terbear@gmail.com>
2018-08-07 02:25:48 -0700

    Finished Textbox component and login UI

--- src/Components.js
+++ src/Components.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
 export class LoadingSpinner extends Component {
 	render() {
 		return <div className={'loading-spinner' + (this.props.on ? ' on' : '')} />
+	}
+}
+
+export class Logo extends Component {
+	render() {
+		return <img className="logo" src="../assets/logo.png" />
 	}
 }
 
@@ -47,6 +54,78 @@ export class Button extends Component {
 				className="button">
 				{this.props.text}
 				<LoadingSpinner on={this.state.loading} />
+			</div>
+		)
+	}
+}
+
+export class Textbox extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = { errored: false, text: this.props.text || '' }
+		this.blocker = React.createRef()
+
+		setTimeout(() => {
+			const width = ReactDOM.findDOMNode(
+				this.refs.label
+			).getBoundingClientRect().width
+			if (this.state.text.length > 0)
+				this.blocker.current.style.width = 'calc(' + width + 'px - 0.7em)'
+		}, 200)
+	}
+
+	setText(text) {
+		this.setState({ text: text })
+	}
+
+	setErrored(errored) {
+		this.setState({ errored: errored })
+	}
+
+	getText() {
+		return this.state.text
+	}
+
+	getErrored() {
+		return this.state.errored
+	}
+
+	handleFocus = () => {
+		const width = ReactDOM.findDOMNode(this.refs.label).getBoundingClientRect()
+			.width
+		if (this.state.text.length > 0)
+			this.blocker.current.style.width = 'calc(' + width + 'px - 0.7em)'
+		else
+			this.blocker.current.style.width =
+				'calc(' + (width * 14) / 16 + 'px + 1em)'
+	}
+
+	handleBlur = () => {
+		if (this.state.text.length === 0) this.blocker.current.style.width = '0em'
+	}
+
+	handleChange = e => {
+		this.setState({ text: e.target.value })
+	}
+
+	render() {
+		const style = { ...this.props.style }
+		style.marginTop = '1em'
+		return (
+			<div style={style} className="textbox">
+				<input
+					onFocus={this.handleFocus}
+					onBlur={this.handleBlur}
+					type={this.props.type || 'text'}
+					className="inputText"
+					onChange={this.handleChange}
+					required
+				/>
+				<span ref="label" className="floating-label">
+					{this.props.hint}
+				</span>
+				<div ref={this.blocker} className="blocker" />
 			</div>
 		)
 	}
--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-import { Button } from './Components'
+import { Button, Logo, Textbox } from './Components'
 
 export default class LoginPage extends Component {
 	render() {
 		return (
 			<div className="cover fullsize allcenter">
+				<img
+					style={{ height: '4em', marginBottom: '1em' }}
+					src="../assets/logo.png"
+				/>
+				<Textbox hint="email" />
+				<Textbox hint="password" type="password" />
 				<Button text="login" triggerLoadOnClick={true} />
+				<Logo />
 			</div>
 		)
 	}
