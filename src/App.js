4c4b2b92675881c3754b7648ff7007a64cea1be1
Terrance Li <terranceli.terbear@gmail.com>
2018-08-06 23:40:39 -0700

    Boilerplate for React app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// eslint-disable-next-line
+String.prototype.isValidEmail = function() {
+	// eslint-disable-next-line
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+	return re.test(this.toLowerCase())
+}
+
+// eslint-disable-next-line
+String.prototype.isOnlyWhitespace = function() {
+	if (this === '') return true
+	return this.replace(/\s/g, '').length === 0
+}
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<Switch>
+					{/* <Route exact path="/" component={HomePage} />
+					<Route component={LostPage} /> */}
+				</Switch>
+			</Router>
+		)
+	}
+}
