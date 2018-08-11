ccfdcb9abff82a4627469003507a7c6781252208
Terrance Li <terranceli.terbear@gmail.com>
2018-08-11 16:00:49 -0700

    Refactor component hierarchy scaffolding

--- src/Grades/SingleClassPage.js
+++ src/Grades/SingleClassPage.js
@@ -89,3 +89,328 @@ export default class SingleClassPage extends Component {
 		)
 	}
 }
+
+class CourseInfoPane extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			page: 'assignments',
+
+			// should stay empty until there are modifications to the assignments
+			projectedAssignments: []
+		}
+
+		this.assignmentsPane = React.createRef()
+	}
+
+	handleProjectionChange = projectedAssignments => {
+		this.setState({ projectedAssignments: projectedAssignments })
+	}
+
+	handleProjectionReset = () => {
+		// TODO: implement projection reset
+		this.assignmentsPane.current.resetProjection()
+		this.setState({ projectedAssignments: [] })
+	}
+
+	render() {
+		return (
+			<div className="course-info-pane">
+				<div className="type-selector">
+					<h3
+						onClick={() => this.setState({ page: 'assignments' })}
+						className={this.state.page === 'assignments' ? 'highlight' : ''}>
+						Assignments
+					</h3>
+					<h3
+						onClick={() => this.setState({ page: 'categories' })}
+						className={this.state.page === 'categories' ? 'highlight' : ''}>
+						Categories
+					</h3>
+				</div>
+
+				{this.state.page === 'assignments' && (
+					<AssignmentsPane
+						ref={this.assignmentsPane}
+						assignments={this.props.assignments}
+						onProjectionChange={this.handleProjectionChange}
+					/>
+				)}
+
+				{this.state.page === 'categories' && (
+					<CategoriesPane categories={this.props.categories} />
+				)}
+
+				<FinalGrades
+					assignments={this.props.assignments}
+					categories={this.props.categories}
+					projectedAssignments={this.state.projectedAssignments}
+				/>
+			</div>
+		)
+	}
+}
+class FinalGrades extends Component {
+	getGrades = (assignments, categories, projectedAssignments) => {
+		var grade = 0
+		var projected = 0
+
+		categories.forEach(category => {
+			var points = 0
+			var total = 0
+			assignments.forEach(assignment => {
+				if (assignment.category === category.name) {
+					points += assignment.score
+					total += assignment.available
+				}
+			})
+			if (total === 0) {
+				grade += category.weight
+			} else {
+				grade += (category.weight * points) / total
+			}
+
+			var projectedPoints = 0
+			var projectedTotal = 0
+			projectedAssignments.forEach(assignment => {
+				if (assignment.category === category.name) {
+					projectedPoints += assignment.score
+					projectedTotal += assignment.available
+				}
+			})
+
+			if (projectedTotal === 0) {
+				projected += category.weight
+			} else {
+				projected += (category.weight * projectedPoints) / projectedTotal
+			}
+		})
+
+		return {
+			grade: (grade * 100).toFixed(1),
+			projected: (projected * 100).toFixed(1)
+		}
+	}
+
+	render() {
+		var grades = this.getGrades(
+			this.props.assignments,
+			this.props.categories,
+			this.props.projectedAssignments
+		)
+		return (
+			<div className="final-grades">
+				<h1>{grades.grade}</h1>
+				{this.props.projectedAssignments.length > 0 && (
+					<div>
+						<h4 className="highlight">
+							{grades.projected - grades.grade > 0
+								? '+' + (grades.projected - grades.grade).toFixed(1)
+								: (grades.projected - grades.grade).toFixed(1)}
+						</h4>
+						<h4 className="highlight">
+							<span className="unselected">projected:</span> {grades.projected}
+						</h4>
+					</div>
+				)}
+			</div>
+		)
+	}
+}
+
+class AssignmentsPane extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			filter: '',
+			assignments: this.props.assignments || [],
+			expandedAssignment: null
+		}
+	}
+
+	componentWillReceiveProps(newProps) {
+		this.setState({ assignments: newProps.assignments })
+	}
+
+	handleFilterChange = text => {
+		this.setState({ filter: text })
+	}
+
+	handleProjectionChange = change => {
+		// TODO: modify this.state, call props handleProjectionChange with new this.state.assignments
+	}
+
+	handleClick = index => {
+		if (this.state.expandedAssignment !== null) {
+			if (this.state.expandedAssignment == index) {
+				this.setState({ expandedAssignment: null })
+			} else {
+				this.setState({ expandedAssignment: index })
+			}
+		} else {
+			this.setState({ expandedAssignment: index })
+		}
+	}
+
+	resetProjection = () => {}
+
+	render() {
+		var filter = this.state.filter
+		var shownAssignments = this.state.assignments.slice().filter(
+			a =>
+				a.name.toLowerCase().includes(filter.toLowerCase()) ||
+				a.score.toString().includes(filter.toLowerCase()) ||
+				a.available.toString().includes(filter.toLowerCase()) ||
+				((a.score / a.available) * 100)
+					.toFixed(1)
+					.toString()
+					.includes(filter.toLowerCase()) ||
+				a.comments.toLowerCase().includes(filter.toLowerCase()) ||
+				a.category.toLowerCase().includes(filter.toLowerCase()) ||
+				a.date.toLowerCase().includes(filter.toLowerCase())
+		)
+		return (
+			<div>
+				<Textbox
+					inputStyle={{ width: '19.4em' }}
+					style={{ marginBottom: '1em' }}
+					hint="filter"
+					onTextChange={this.handleFilterChange}
+				/>
+				{/* TODO: INSERT ASSIGNMENT CREATION MEME */}
+				{shownAssignments.map((assignment, index) => {
+					return (
+						<Assignment
+							// TODO: INCLUDE ASSIGNMENT EDITING MEME
+							expanded={this.state.expandedAssignment === index}
+							handleClick={() => this.handleClick(index)}
+							date={assignment.date}
+							name={assignment.name}
+							score={assignment.score}
+							available={assignment.available}
+							category={assignment.category}
+							comments={assignment.comments}
+						/>
+					)
+				})}
+			</div>
+		)
+	}
+}
+
+class Assignment extends Component {
+	constructor(props) {
+		super(props)
+
+		this.commentsDiv = React.createRef()
+	}
+
+	componentWillReceiveProps(newProps) {
+		var expanded = newProps.expanded
+
+		if (expanded) {
+			const height = ReactDOM.findDOMNode(
+				this.refs.comments
+			).getBoundingClientRect().height
+			this.commentsDiv.current.style.height = 'calc(' + height + 'px + 3em)'
+		} else {
+			this.commentsDiv.current.style.height = '0'
+		}
+	}
+
+	render() {
+		return (
+			<div className="assignment">
+				<div className="assignment-main" onClick={this.props.handleClick}>
+					<h5 className="assignment-date">{this.props.date}</h5>
+					<svg height="12" width="12" style={{ paddingTop: '0.6em' }}>
+						<circle cx="6" cy="6" r="6" fill="#12EB9D" />
+					</svg>
+					<div className="assignment-info">
+						<div className="assignment-name-grade">
+							<h4 className="assignment-name">{this.props.name}</h4>
+							<h4 className="assignment-grade">
+								{((this.props.score / this.props.available) * 100).toFixed(1)}
+							</h4>
+						</div>
+						<div className="assignment-category">
+							<h5>{this.props.category}</h5>
+							<h5>{this.props.score + '/' + this.props.available}</h5>
+						</div>
+					</div>
+				</div>
+				<div
+					ref={this.commentsDiv}
+					className={
+						'assignment-comments' + (this.props.expanded ? ' expanded' : '')
+					}>
+					<div className="assignment-comments-content">
+						<h4>Comments</h4>
+						<p ref="comments">{this.props.comments}</p>
+					</div>
+					<div className="assignment-controls">
+						<i className="material-icons assignment-action">delete</i>
+						<i className="material-icons assignment-action">autorenew</i>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+class CategoriesPane extends Component {
+	render() {
+		return (
+			<div>
+				{this.props.categories.map((category, index) => {
+					return (
+						<Category
+							name={category.name}
+							score={category.score}
+							available={category.available}
+							weight={category.weight}
+						/>
+					)
+				})}
+			</div>
+		)
+	}
+}
+
+class Category extends Component {
+	render() {
+		return (
+			<div className="category">
+				<div className="category-name-grade">
+					<h4 className="category-name">{this.props.name}</h4>
+					<h4 className="category-grade">
+						{this.props.available === 0
+							? '100.0%'
+							: ((this.props.score / this.props.available) * 100).toFixed(1) +
+							  '%'}
+					</h4>
+				</div>
+				<div className="category-details">
+					<h5>{this.props.score + '/' + this.props.available}</h5>
+					<h5>
+						{this.props.available === 0
+							? (this.props.weight * 100).toFixed(1) +
+							  '/' +
+							  (this.props.weight * 100).toFixed(1) +
+							  '%'
+							: (
+									(this.props.score / this.props.available) *
+									this.props.weight *
+									100
+							  ).toFixed(1) +
+							  '/' +
+							  (this.props.weight * 100).toFixed(1) +
+							  '%'}
+					</h5>
+				</div>
+			</div>
+		)
+	}
+}
