937e24b5b1299becd7d84ca86c19644eea3bad2b
Terrance Li <terranceli.terbear@gmail.com>
2018-12-28 17:35:30 -0800

    Integrated updated NPM module

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 import { Button, Logo, Textbox } from './Components'
+import gb from '@team-llambda/gradebook-api'
 
 export default class LoginPage extends Component {
 	constructor(props) {
 		super(props)
 
 		//TODO: call reauth route to see if already authenticated
+
+		this.usernameTextbox = React.createRef()
+		this.passwordTextbox = React.createRef()
+	}
+
+	login = async () => {
+		let username = this.usernameTextbox.current.getText()
+		let password = this.passwordTextbox.current.getText()
+		let res = await gb.login(username, password)
+		console.log(res)
 	}
 
 	render() {
@@ -16,9 +27,9 @@ export default class LoginPage extends Component {
 					style={{ height: '4em', marginBottom: '1em' }}
 					src="../assets/logo.svg"
 				/>
-				<Textbox hint="username" />
-				<Textbox hint="password" type="password" />
-				<Button text="login" triggerLoadOnClick={true} />
+				<Textbox hint="username" ref={this.usernameTextbox} />
+				<Textbox hint="password" type="password" ref={this.passwordTextbox} />
+				<Button text="login" triggerLoadOnClick={true} onClick={this.login} />
 				<Logo />
 			</div>
 		)
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log(require('@team-llambda/gradebook-api'))
