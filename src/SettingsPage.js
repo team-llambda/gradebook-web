11ad565b27adf68d9e1ae27baa1cfc5e235e311e
Terrance Li <terranceli.terbear@gmail.com>
2019-01-09 12:38:54 -0800

    Optimized drop-shadow

--- src/SettingsPage.js
+++ src/SettingsPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Menu, Logo } from './Components'
+import { Menu, Logo, Button } from './Components'
 
 export default class SettingsPage extends Component {
 	render() {
@@ -8,6 +8,26 @@ export default class SettingsPage extends Component {
 				<Menu currentItemIndex={2} />
 				<div className="content">
 					<h1>Settings</h1>
+					<div className="card deactivate">
+						<h3 style={{ color: 'white', cursor: 'default' }}>
+							Deactivate Account
+						</h3>
+						<h4 style={{ color: 'white' }}>
+							The Gradebook team uses industry-standard security practices, and
+							your information is secure inside our system. However, if you
+							still feel uncomfortable about using Gradebook, you may delete
+							your Gradebook account and we will wipe all of our records about
+							your user.
+						</h4>
+						<Button
+							style={{
+								background: 'white',
+								color: '#f05056'
+							}}
+							text="Deactivate"
+							onClick={this.deactivate}
+						/>
+					</div>
 				</div>
 				<Logo />
 			</div>
