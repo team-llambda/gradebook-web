f2663506d8dbbf2d86f7b6fa5fd4089f840d71a3
Terrance Li <terranceli.terbear@gmail.com>
2019-01-09 12:13:31 -0800

    Close dropdown on click elsewhere

--- src/Components.js
+++ src/Components.js
@@ -464,7 +464,12 @@ export class Dropdown extends Component {
 
 	render() {
 		return (
-			<div tabIndex="0" onBlur={this.setState({open: false})}>
+			<div
+				style={{ outline: 'none' }}
+				tabIndex="0"
+				onBlur={() => {
+					this.setState({ open: false })
+				}}>
 				<h5
 					onClick={() => this.setState({ open: !this.state.open })}
 					className={this.props.highlight ? 'highlight' : ''}
